37798c8d91ea027269cecf12bf8d4de4d4cdd592
HavertzZ04 <havertzz04@gmail.com>
2023-04-19 21:37:12 -0500

    feat: :construction: Add and modify pagination

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
 const pokemonContainer = document.querySelector(".pokemon-container");
 const spinner = document.querySelector("#spinner")
+const previous = document.querySelector("#previous");
+const next = document.querySelector("#next");
+
+let offset = 1;
+let limit = 11;
+
+previous.addEventListener("click", () => {
+    if (offset != 1){
+        offset -= 12;
+        removeChildNodes(pokemonContainer);
+        fetchPokemons(offset, limit);
+    }
+});
+
+next.addEventListener("click", () => {
+    offset += 12;
+    removeChildNodes(pokemonContainer);
+    fetchPokemons(offset, limit);
+});
 
 function fetchPokemon(id){
     fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
@@ -10,9 +29,9 @@ function fetchPokemon(id){
     })
 }
 
-function fetchPokemons(number){
+function fetchPokemons(offset, limit) {
     spinner.style.display = "block";
-    for(let i = 1; i <= number; i++){
+    for(let i = offset; i <= offset + limit; i++){
         fetchPokemon(i)
     }
 }
@@ -44,4 +63,10 @@ function createPokemon(pokemon){
 
 }
 
-fetchPokemons(12)
+function removeChildNodes(parent){
+    while(parent.firstChild){
+        parent.removeChild(parent.firstChild);
+    };
+}
+
+fetchPokemons(offset, limit)

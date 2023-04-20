b0db4961c01ab68b13ebeab588011c9eab0cdffa
HavertzZ04 <havertzz04@gmail.com>
2023-04-19 23:08:14 -0500

    feat: :arrow_up: Flip Cards
    Allow to see the pokemons stats using hover in each card

--- main.js
+++ main.js
@@ -36,37 +36,87 @@ function fetchPokemons(offset, limit) {
     }
 }
 
-function createPokemon(pokemon){
-    const card = document.createElement("div");
-    card.classList.add("pokemon-block");
+function createPokemon(pokemon) {
+  const flipCard = document.createElement("div");
+  flipCard.classList.add("flip-card");
 
-    const spriteContainer = document.createElement("div");
-    spriteContainer.classList.add("img-container");
+  const cardContainer = document.createElement("div");
+  cardContainer.classList.add("card-container");
 
-    const sprite = document.createElement("img");
-    sprite.src = pokemon.sprites.front_default
+  flipCard.appendChild(cardContainer);
 
-    spriteContainer.appendChild(sprite);
+  const card = document.createElement("div");
+  card.classList.add("pokemon-block");
 
-    const number = document.createElement("p");
-    number.textContent = `#${pokemon.id.toString().padStart(3, 0)}`;
+  const spriteContainer = document.createElement("div");
+  spriteContainer.classList.add("img-container");
 
-    const name = document.createElement("p");
-    name.classList.add("name");
-    name.textContent = pokemon.name
+  const sprite = document.createElement("img");
+  sprite.src = pokemon.sprites.front_default;
 
-    card.appendChild(spriteContainer);
-    card.appendChild(number);
-    card.appendChild(name);
+  spriteContainer.appendChild(sprite);
 
-    pokemonContainer.appendChild(card)
+  const number = document.createElement("p");
+  number.textContent = `#${pokemon.id.toString().padStart(3, 0)}`;
 
+  const name = document.createElement("p");
+  name.classList.add("name");
+  name.textContent = pokemon.name;
+
+  card.appendChild(spriteContainer);
+  card.appendChild(number);
+  card.appendChild(name);
+
+  const cardBack = document.createElement("div");
+  cardBack.classList.add("pokemon-block-back");
+
+  cardBack.appendChild(progressBars(pokemon.stats));
+
+  cardContainer.appendChild(card);
+  cardContainer.appendChild(cardBack);
+  pokemonContainer.appendChild(flipCard);
 }
 
-function removeChildNodes(parent){
-    while(parent.firstChild){
-        parent.removeChild(parent.firstChild);
-    };
+function progressBars(stats) {
+  const statsContainer = document.createElement("div");
+  statsContainer.classList.add("stats-container");
+
+  for (let i = 0; i < 3; i++) {
+    const stat = stats[i];
+
+    const statPercent = stat.base_stat / 2 + "%";
+    const statContainer = document.createElement("stat-container");
+    statContainer.classList.add("stat-container");
+
+    const statName = document.createElement("p");
+    statName.textContent = stat.stat.name;
+
+    const progress = document.createElement("div");
+    progress.classList.add("progress");
+
+    const progressBar = document.createElement("div");
+    progressBar.classList.add("progress-bar");
+    progressBar.setAttribute("aria-valuenow", stat.base_stat);
+    progressBar.setAttribute("aria-valuemin", 0);
+    progressBar.setAttribute("aria-valuemax", 255);
+    progressBar.style.width = statPercent;
+
+    progressBar.textContent = stat.base_stat;
+
+    progress.appendChild(progressBar);
+    statContainer.appendChild(statName);
+    statContainer.appendChild(progress);
+
+    statsContainer.appendChild(statContainer);
+  }
+
+  return statsContainer;
 }
 
-fetchPokemons(offset, limit)
+function removeChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+fetchPokemons(offset, limit);

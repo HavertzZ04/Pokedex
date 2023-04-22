b05609f84ab4b9864e75c8777138e6f31647dcb7
HavertzZ04 <havertzz04@gmail.com>
2023-04-22 01:54:34 -0500

    feat: :sparkles: Create the app file
    I created the app.js file in order to import all the project's modules

---
+++ app.js
@@ -0,0 +1,20 @@
+import mySearcher from "./components/mySearcher.js";
+import myButtons from "./components/myButtons.js";
+import myMusic from "./components/myMusic.js";
+import myPagination from "./components/myPagination.js";
+import myFooter from "./components/myFooter.js";
+
+import {music} from "./logic/myMusic.js";
+import {logic} from "./logic/myGetPokemon.js"
+import { buttons } from "./logic/myButtons.js";
+
+
+mySearcher.show();
+myButtons.show();
+myMusic.show();
+myPagination.show();
+myFooter.show();
+
+music();
+logic();
+buttons();

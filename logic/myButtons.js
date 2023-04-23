8f7d6706e793b45efb5e9faf994a70a34cdd03bb
HavertzZ04 <havertzz04@gmail.com>
2023-04-23 18:43:07 -0500

    feat: :zap: Add the bottons
    This code is not added on the app.js file yet

--- logic/myButtons.js
+++ logic/myButtons.js
@@ -1,8 +1,24 @@
 export function buttons() {
 
     let bug = document.querySelector("#bug");
+    let dark = document.querySelector("#dark");
+    let electric = document.querySelector("#electric");
+    let fairy = document.querySelector("#fairy");
+    let dragon = document.querySelector("#dragon");
+    let grass = document.querySelector("#grass");
+    let fire = document.querySelector("#fire");
+    let flying = document.querySelector("#flying");
+    let ice = document.querySelector("#ice");
+    let rock = document.querySelector("#rock");
+    let fighting = document.querySelector("#fighting");
+    let ghost = document.querySelector("#ghost");
+    let poison = document.querySelector("#poison");
+    let normal = document.querySelector("#normal");
+    let psychic = document.querySelector("#psychic");
+    let ground = document.querySelector("#ground");
+    let steel = document.querySelector("#steel");
+    let water = document.querySelector("#water");
 
-    bug.addEventListener("click", () => {
-        console.log("45");
-    })
+
+
 }

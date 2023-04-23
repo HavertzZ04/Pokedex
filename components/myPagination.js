466d49805a3e55ec69b03f028352e8f4775d72f8
HavertzZ04 <havertzz04@gmail.com>
2023-04-23 18:46:21 -0500

    feat: :zap: Modulate

--- components/myPagination.js
+++ components/myPagination.js
@@ -8,10 +8,10 @@ export default{
         `
         <ul class="pagination justify-content-center">
             <li class="page-item" id="previous">
-                <a class="page-link" href="#" aria-label="Previous">Previous</a>
+                <a class="page-link" href="#" aria-label="${this.previous}">Previous</a>
             </li>
             <li class="page-item" id="next">
-                <a class="page-link" href="#" aria-label="Next">Next</a>
+                <a class="page-link" href="#" aria-label="Next">${this.next}</a>
             </li>
         </ul>
         `

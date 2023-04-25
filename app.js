fda4dda5ce7318f892ecc54411bda06cc9c3c8b1
HavertzZ04 <havertzz04@gmail.com>
2023-04-25 04:42:34 -0500

    feat: :coffin: Remove buttons modules
    I removed all the buttons fuctions due to I wasn't able to complete the necessary code on time

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
 import mySearcher from "./components/mySearcher.js";
-//import myButtons from "./components/myButtons.js";
 import myMusic from "./components/myMusic.js";
 import myPagination from "./components/myPagination.js";
 import myFooter from "./components/myFooter.js";
 
 import {music} from "./logic/myMusic.js";
 import {logic} from "./logic/myGetPokemon.js"
-//import {buttons} from "./logic/myButtons.js";
-
 
 mySearcher.show();
-//myButtons.show();
 myMusic.show();
 myPagination.show();
 myFooter.show();
 
 music();
 logic();
-//buttons();

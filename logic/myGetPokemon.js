8ae8785a5101bbbdf81e40452d3ae7881b9f9708
havertzz04 <havertzz04@gmail.com>
2023-04-26 07:17:05 -0500

    feat: :arrow_up: Add Workers
    I modified the code in order to spedite how the code show all the cards and search on the bar, using service workers

--- logic/myGetPokemon.js
+++ logic/myGetPokemon.js
@@ -1,61 +1,19 @@
 export function logic(){
   let pokemonContainer = document.querySelector(".pokemon-container");
 
-  let getPokemon = async (id) =>{
-    let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-    let data = await response.json();
-    createPokemon(data);
-  }
+  let ws = new Worker("./storage/wsGetPokemon.js")
   
-  let createPokemon = (pokemon) => {
-    let flipCard = `
-      <div class="flip-card">
-          <div class="card-container">
-            <div class="pokemon-block">
-              <div class="img-container">
-                <img class="pokeImage" src="${pokemon.sprites.other.home.front_default}">
-              </div>
-              <p>${`#${pokemon.id.toString().padStart(3, 0)}`}</p>
-              <p class="name">${pokemon.name}</p>
-            </div>
-          <div class="pokemon-block-back">
-            ${progressBars(pokemon.stats)}
-          </div>
-        </div>
-      </div>
-      `;
-    pokemonContainer.insertAdjacentHTML("beforeend", flipCard);
-  }
-  
-  let progressBars = (stats) => {
-    let statsContainer = `<div class="stats-container">`;
-  
-    for (let i = 0; i < 3; i++) {
-      let stat = stats[i];
-      let statPercent = stat.base_stat / 2 + "%";
-      let statContainer = `
-        <div class="stat-container">
-          <p>${stat.stat.name}</p>
-          <div class="progress">
-            <div class="progress-bar" aria-valuenow="${stat.base_stat}" aria-valuemin="0" aria-valuemax="255" style="width: ${statPercent}">
-              ${stat.base_stat}
-            </div>
-          </div>
-        </div>
-        `;
-      statsContainer += statContainer;
-    }
-    statsContainer += `</div>`;
-    return statsContainer;
-  }
-  
-  // Pagination
   let getPokemons = (offset, limit) => {
     for (let i = offset; i <= offset + limit; i++) {
-      getPokemon(i);
+      ws.postMessage(i)
     }
   }
-  
+  pokemonContainer.innerHTML = ""
+  ws.addEventListener("message",(e) =>{
+    pokemonContainer.insertAdjacentHTML("beforeend", e.data)
+  })
+
+  // Pagination
   let previous = document.querySelector("#previous");
   let next = document.querySelector("#next");
   let offset = 1;
@@ -82,29 +40,23 @@ export function logic(){
   }
   getPokemons(offset, limit);
   
-  // Barra de búsqueda en vivo
+  //Search Bar
   let searchInput = document.querySelector("#search-bar");
+  const ws2 = new Worker("./storage/wsSearchBar.js")
   
   searchInput.addEventListener("input", (event) => {
     let searchTerm = event.target.value.toLowerCase();
     removeChildNodes(pokemonContainer);
     if (searchTerm.length > 0) {
-      searchPokemons(searchTerm);
+      ws2.postMessage(searchTerm)
     } else {
       getPokemons(offset, limit);
     }
   });
   
-  let searchPokemons = async (term) => {
-    let response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=905`);
-    let data = await response.json();
-    let pokemons = data.results;
-    let matchingPokemons = pokemons.filter(pokemon => pokemon.name.startsWith(term));
-    matchingPokemons.forEach(async (pokemon) => {
-      let response = await fetch(pokemon.url);
-      let data = await response.json();
-      createPokemon(data);
-    });
-  };
-  
+  ws2.addEventListener("message",(e) =>{
+    pokemonContainer.insertAdjacentHTML("beforeend", e.data)
+  })
 }
+
+  
